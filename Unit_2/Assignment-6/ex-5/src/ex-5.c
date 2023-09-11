522aacd0a681b49e34f8a2192a2e60ae3837a5fd
Seif Hasseb <137090440+SeifHasseb@users.noreply.github.com>
2023-09-11 19:13:21 +0300

    Pointers

--- Unit_2/Assignment-6/ex-5/src/ex-5.c
+++ Unit_2/Assignment-6/ex-5/src/ex-5.c
@@ -9,11 +9,19 @@
 */
 #include <stdio.h>
 #include <stdlib.h>
+<<<<<<< HEAD
+=======
+
+>>>>>>> 7a14721 (Pointers)
 #include <stdio.h>
 struct Exmployee{
 	char name[10];
 	int id;
 };
+<<<<<<< HEAD
+=======
+
+>>>>>>> 7a14721 (Pointers)
 int main(void) {
 	setvbuf(stdout, NULL, _IONBF, 0);	//Eclipse bug
 	struct Exmployee emp1 = {"Ahamed" ,31},emp2= {"Mohamed",32};
