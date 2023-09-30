3be2cf1aa1b69f4196a513ed9e4f62f8ae012907
Seif Hasseb <137090440+SeifHasseb@users.noreply.github.com>
2023-09-30 11:27:52 +0300

    Lab_1

--- Unit_3/lesson-2/lab_1/app.c
+++ Unit_3/lesson-2/lab_1/app.c
@@ -1,5 +1,8 @@
 # include "uart.h"
 unsigned char string_buffer[100]="Learn-in-depth:Seif";
+unsigned char string_buffer2[100]="Learn-in-depth:Seif2";
+unsigned char string_buffer4[100]="Learn-in-depth:Seif4";
+
 void main (){
 
 	Uart_Send_String(string_buffer);
